3840bb7fa86b81344b0a44feb5008ce971401569
Ben Medcalf <ben.medcalf@gmail.com>
2023-10-08 13:00:01 -0400

    fix none option issue

--- starter/src/Book.js
+++ starter/src/Book.js
@@ -22,12 +22,13 @@ const Book = ({ book, moveBookToShelf }) => {
         ></div>
         <div className="book-shelf-changer">
           <select onChange={onMoveToShelf} defaultValue={shelf}>
-            <option value="none" disabled>
+            <option disabled>
               Move to...
             </option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
             <option value="read">Read</option>
+            <option value="none">None</option>
           </select>
         </div>
       </div>
